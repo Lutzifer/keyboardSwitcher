f4b4a3ec31722ffe3ec277ebfdc4a967c379a634
Wolfgang Lutz <wolfgang@lutz-wiesent.de>
2015-09-28 22:08:31 +0200

    Initial Commit

---
+++ keyboardSwitcher/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  keyboardSwitcher
+//
+//  Created by Wolfgang Lutz on 28.09.15.
+//  Copyright © 2015 number42. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
